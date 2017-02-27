69dc045cb8afb4f776dfbfd0e3ab40d436f7d497
aaron mathankeri <aaron@quantifiedag.com>
2017-02-27 15:31:09 -0600

    restructured code so it is more concise and flows better. No development added, just pieces moved around.

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
       cout << "Learning rate is " << EPSILON << endl;
       cout << "Regularizer strength is " << LAMBDA << endl;
       //--------------------------------------------------------------------
+      double * features = (double *)mkl_malloc( NUM_FEATURES*sizeof( double ), 64 );
       double * targets = (double *)mkl_malloc( NUM_SAMPLES*sizeof( double ), 64 );
       double * finalOutputs = (double *)mkl_malloc( NUM_SAMPLES*sizeof( double ), 64 );
 
@@ -34,18 +35,15 @@ int main(int argc, char *argv[])
 
       cout << "finalOutputs are :" << endl;
       printMatrix( finalOutputs, 1, NUM_SAMPLES);
+      //test error function
       errorFunction( targets , finalOutputs );
       //--------------------------------------------------------------------
-      //double * features = (double *)mkl_malloc( NUM_SAMPLES* NUM_FEATURES*sizeof( double ), 64 );
-      double * features = (double *)mkl_malloc( NUM_FEATURES*sizeof( double ), 64 );
-      //initializeMatrix( features, NUM_SAMPLES, NUM_FEATURES );
-
       //set features for now
       features[0]	=  0.74346118;  features[1]  =  0.46465633;
       cout << "Features are " << endl;
       printMatrix( features, 1, NUM_FEATURES );
-
       //--------------------------------------------------------------------
+      //test activation function
       activationFunction( features );
       //--------------------------------------------------------------------
       //--------------------------------------------------------------------
@@ -86,7 +84,7 @@ void activationFunction( double *a ){
 	    z[i] = 1/( 1 + a[i]);
       }
 
-      cout << "z is " << endl;
+      cout << "Non-linear transformation complete. Z is " << endl;
       printMatrix( z , 1 , NUM_FEATURES);
 }
 
@@ -112,21 +110,24 @@ void errorFunction( double *targets, double *finalOutputs){
 
       cout << "Error = " << res << endl;
 }
-      /*
-      features[2]	=  1.65755662;  features[3]  = -0.63203157;
-      features[4]	= -0.15878875;  features[5]  =  0.25584465;
-      features[6]	= -1.088752  ;  features[7]  = -0.39694315;
-      features[8]	=  1.768052  ;  features[9]  = -0.25443213;
-      features[10]	=  1.95416454;  features[11] = -0.12850579;
-      features[12]	=  0.93694537;  features[13] =  0.36597075;
-      features[14]	=  0.88446589;  features[15] = -0.47595401;
-      features[16]	=  0.80950246;  features[17] =  0.3505231 ;
-      features[18]	=  1.2278091;   features[19] = -0.64785108;
-      */
-      /*
-      targets[1] = 1; targets[6] = 0;
-      targets[2] = 1; targets[7] = 1;
-      targets[3] = 0; targets[8] = 0;
-      targets[4] = 1; targets[9] = 1;
-      */
+//double * features = (double *)mkl_malloc( NUM_SAMPLES* NUM_FEATURES*sizeof( double ), 64 );
+//initializeMatrix( features, NUM_SAMPLES, NUM_FEATURES );
+
+/*
+  features[2]	=  1.65755662;  features[3]  = -0.63203157;
+  features[4]	= -0.15878875;  features[5]  =  0.25584465;
+  features[6]	= -1.088752  ;  features[7]  = -0.39694315;
+  features[8]	=  1.768052  ;  features[9]  = -0.25443213;
+  features[10]	=  1.95416454;  features[11] = -0.12850579;
+  features[12]	=  0.93694537;  features[13] =  0.36597075;
+  features[14]	=  0.88446589;  features[15] = -0.47595401;
+  features[16]	=  0.80950246;  features[17] =  0.3505231 ;
+  features[18]	=  1.2278091;   features[19] = -0.64785108;
+*/
+/*
+  targets[1] = 1; targets[6] = 0;
+  targets[2] = 1; targets[7] = 1;
+  targets[3] = 0; targets[8] = 0;
+  targets[4] = 1; targets[9] = 1;
+*/
 
